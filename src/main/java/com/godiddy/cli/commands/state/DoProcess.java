bc74ba9a764732c464179677769e762fc6a0a614
Markus Sabadello <markus@danubetech.com>
2025-12-15 15:35:45 +0100

    Use danubetech-registrar-common-java 0.2.0

--- src/main/java/com/godiddy/cli/commands/state/DoProcess.java
+++ src/main/java/com/godiddy/cli/commands/state/DoProcess.java
@@ -2,16 +2,11 @@ package com.godiddy.cli.commands.state;
 
 import com.danubetech.kms.clientkeyinterface.ClientKeyInterface;
 import com.danubetech.kms.clientstateinterface.ClientStateInterface;
-import com.danubetech.uniregistrar.local.extensions.handlers.HandleStateUpdateTempKeys;
-import com.danubetech.uniregistrar.local.extensions.handlers.HandleStateUpdateVerificationMethods;
-import com.danubetech.uniregistrar.local.extensions.handlers.action.HandleActionState;
-import com.danubetech.uniregistrar.local.extensions.handlers.finished.HandleFinishedStateImportSecretJsonWebKeys;
-import com.danubetech.uniregistrar.local.extensions.handlers.finished.HandleFinishedStateImportSecretVerificationMethods;
+import com.danubetech.uniregistrar.local.internalsecretmode.InternalSecretMode;
 import com.godiddy.api.client.openapi.model.RegistrarRequest;
 import com.godiddy.api.client.openapi.model.RegistrarResourceState;
 import com.godiddy.api.client.openapi.model.RegistrarState;
 import com.godiddy.cli.clistorage.clistate.CLIState;
-import com.godiddy.cli.commands.registrar.ContinueCommand;
 import com.godiddy.cli.commands.registrar.DoContinue;
 import com.godiddy.cli.config.Api;
 import com.godiddy.cli.interfaces.Interfaces;
@@ -44,25 +39,17 @@ public class DoProcess {
         RegistrarRequest nextRequest = null;
 
         if (state instanceof RegistrarState registrarState) {
+            uniregistrar.openapi.model.RegistrarRequest handleRequest = MappingUtil.map(prevRequest);
             uniregistrar.openapi.model.RegistrarState handleState = MappingUtil.map(registrarState);
-            uniregistrar.openapi.model.RegistrarRequest handlePrevRequest = MappingUtil.map(prevRequest);
 
-            HandleStateUpdateVerificationMethods.handleState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
-            HandleStateUpdateTempKeys.handleState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
-            HandleFinishedStateImportSecretJsonWebKeys.handleFinishedState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
-            HandleFinishedStateImportSecretVerificationMethods.handleFinishedState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
-            uniregistrar.openapi.model.RegistrarRequest handleNextRequest = HandleActionState.handleActionState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
+            uniregistrar.openapi.model.RegistrarRequest handleNextRequest = InternalSecretMode.handleState(handleRequest, handleState, clientKeyInterface, clientStateInterface);
 
             nextRequest = MappingUtil.map(handleNextRequest);
         } else if (state instanceof RegistrarResourceState registrarResourceState) {
+            uniregistrar.openapi.model.RegistrarRequest handleRequest = MappingUtil.map(prevRequest);
             uniregistrar.openapi.model.RegistrarResourceState handleState = MappingUtil.map(registrarResourceState);
-            uniregistrar.openapi.model.RegistrarRequest handlePrevRequest = MappingUtil.map(prevRequest);
 
-            HandleStateUpdateVerificationMethods.handleState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
-            HandleStateUpdateTempKeys.handleState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
-            HandleFinishedStateImportSecretJsonWebKeys.handleFinishedState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
-            HandleFinishedStateImportSecretVerificationMethods.handleFinishedState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
-            uniregistrar.openapi.model.RegistrarRequest handleNextRequest = HandleActionState.handleActionState(handlePrevRequest, handleState, clientKeyInterface, clientStateInterface);
+            uniregistrar.openapi.model.RegistrarRequest handleNextRequest = InternalSecretMode.handleState(handleRequest, handleState, clientKeyInterface, clientStateInterface);
 
             nextRequest = MappingUtil.map(handleNextRequest);
         }
